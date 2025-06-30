8aa7b4a17975703a04fc01cb9ff2de716d68bcef
Kien Tran <61492263+kien2020@users.noreply.github.com>
2025-06-30 10:56:57 +0700

    Update LiChaoTree.cpp

--- LiChaoTree.cpp
+++ LiChaoTree.cpp
@@ -1,109 +1,99 @@
 /*
-problem: https://codeforces.com/problemset/problem/932/F
+problem: https://oj.vnoi.info/problem/jewelnb
 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void setup()
+{
+#ifndef ONLINE_JUDGE
+    freopen("test.inp", "r", stdin);
+    freopen("test.out", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
 
 struct LINE
 {
-    long long a, b;
-    inline LINE()
-    {
-        a = 1e9;
-        b = 9e18;
-    }
-    inline LINE(long long new_a, long long new_b)
-    {
-        a = new_a;
-        b = new_b;
-    }
+    long long a = -2e9, b = -2e9;
     inline long long Val(long long x)
     {
         return a * x + b;
     }
 };
 
-struct NODE
+struct LICHAO_TREE
 {
-    LINE v = LINE();
-    int l, r;
-};
-
-struct LICHAOTREE
-{
-    int sz_tree = 0;
-    NODE tree[100000];
-    vector<int> empty_node;
-    inline int New(LINE inp)
+    LINE tree[800000];
+    inline void Add(int ind, int l, int r, LINE v)
     {
-        int ind;
-        if (!empty_node.empty())
+        int m = (l + r) >> 1;
+        if (tree[ind].Val(m) < v.Val(m))
         {
-            ind = empty_node.back();
-            empty_node.pop_back();
-        }
-        else
-        {
-            ind = sz_tree++;
-        }
-        tree[ind].l = tree[ind].r = -1;
-        tree[ind].v = inp;
-        return ind;
-    }
-    inline void Release(int ind)
-    {
-        empty_node.push_back(ind);
-    }
-    inline void Update(int &ind, int l, int r, LINE inp)
-    {
-        if (ind == -1)
-        {
-            ind = New(inp);
-            return;
-        }
-        int m = (l + r) / 2;
-        if (inp.Val(m) < tree[ind].v.Val(m))
-        {
-            swap(inp, tree[ind].v);
+            swap(tree[ind], v);
         }
         if (l < r)
         {
-            if (inp.Val(l) < tree[ind].v.Val(l))
+            if (tree[ind].Val(l) < v.Val(l))
             {
-                Update(tree[ind].l, l, m, inp);
+                Add(ind << 1, l, m, v);
             }
-            if (inp.Val(r) < tree[ind].v.Val(r))
+            if (tree[ind].Val(r) < v.Val(r))
             {
-                Update(tree[ind].r, m + 1, r, inp);
+                Add(ind << 1 | 1, m + 1, r, v);
             }
         }
     }
-    inline long long Get(int ind, int l, int r, long long x)
+    inline void Update(int ind, int l, int r, int x, int y, LINE v)
     {
-        if (ind == -1)
+        if (r < x || y < l)
         {
-            return 9e18;
-        }
-        if (l == r)
-        {
-            return tree[ind].v.Val(x);
-        }
-        int m = (l + r) / 2;
-        if (x <= m)
-        {
-            return min(tree[ind].v.Val(x), Get(tree[ind].l, l, m, x));
-        }
-        return min(tree[ind].v.Val(x), Get(tree[ind].r, m + 1, r, x));
-    }
-    inline void Merge(int &ind, int oth, int l, int r)
-    {
-        if (ind == -1 || oth == -1)
-        {
-            ind = (ind == -1 ? oth : ind);
             return;
         }
-        int m = (l + r) / 2;
-        Update(ind, l, r, tree[oth].v);
-        Merge(tree[ind].l, tree[oth].l, l, m);
-        Merge(tree[ind].r, tree[oth].r, m + 1, r);
-        Release(oth);
+        if (x <= l && r <= y)
+        {
+            Add(ind, l, r, v);
+            return;
+        }
+        int m = (l + r) >> 1;
+        Update(ind << 1, l, m, x, y, v);
+        Update(ind << 1 | 1, m + 1, r, x, y, v);
     }
-};
+    inline long long Get(int ind, int l, int r, int x)
+    {
+        if (l == r)
+        {
+            return tree[ind].Val(x);
+        }
+        int m = (l + r) >> 1;
+        if (x <= m)
+        {
+            return max(tree[ind].Val(x), Get(ind << 1, l, m, x));
+        }
+        return max(tree[ind].Val(x), Get(ind << 1 | 1, m + 1, r, x));
+    }
+} lichao;
+
+long long n, m, a, b, c, d;
+
+int main()
+{
+    setup();
+
+    cin >> n >> m;
+    while (m--)
+    {
+        cin >> a >> b >> d >> c;
+        lichao.Update(1, 1, 2e5, a, b, {c, d - c * a});
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        a = lichao.Get(1, 1, 2e5, i);
+        cout << (a == i * -2e9 - 2e9 ? 0 : a) << "\n";
+    }
+    return 0;
+}
